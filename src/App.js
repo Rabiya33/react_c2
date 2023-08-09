b60e3a464fbfb19aab06346256128c0046a5a1a7
Rabs33 <rabs@33188.com>
2023-08-09 08:15:22 +0545

    -style

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+     <h1 style={{
+      color : "red",
+      padding:"20px",
+      backgroundColor:"black",
+      paddingBottom:"20px"
+
+     }}> Hello world</h1>
+
+
      
     </div>
   );
